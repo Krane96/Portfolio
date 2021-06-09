0e592e10502d9c74d6ee8b190fcb9e19e1d8c8b5
Krane96 <69842760+Krane96@users.noreply.github.com>
2021-06-09 19:04:42 +0200

    dsadas

--- scripts.js
+++ scripts.js
@@ -1,1 +1,35 @@
-//Hover
+//API
+const url = 'https://www.martinlk.no/wp-json/wp/v2/posts?per_page=3';
+const posts = document.querySelector('.latest-blog-posts');
+
+async function fetchPosts(){
+    try{
+        const waiting = await fetch(url);
+        const result = await waiting.json();
+        sendAPItoHTML(result);
+        console.log(result);
+    }
+    catch(error){
+        console.log(error);
+    }
+};
+
+fetchPosts();
+
+//send api to html
+
+function sendAPItoHTML(post){
+    post.forEach(function(blog){
+        posts.innerHTML += `
+        <div class="latestPosts">
+        <a href="#">${blog.title.rendered}</a>
+        <img>${blog.content.rendered}</img>
+
+
+
+        </div>
+        `
+
+    })
+
+};
